d274f30f1dd011a39a9d749e243a55115da901f4
Meng Wang <water.wang@live.com>
2017-01-18 18:54:36 -0600

    index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var appmain = new Vue({
+    el: '#index',
+    data: {
+        message: 'Hello Vue!',
+        showing: true
+    },
+    methods: {
+        login: function() {
+        }
+    }
+});
