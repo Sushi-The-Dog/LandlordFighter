fd58d0d9fbfb8b5c9eca6ece6889a45cccc01dec
Meng Wang <water.wang@live.com>
2017-01-19 13:28:18 -0600

    shadow

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Texas Hold'em Poker Game And AI based on Web
 
+### _@Author WMXPY, Struggler_
+
 _Library - phpsocket.io, workerman_
--- src/indexstyle.js
+++ src/indexstyle.js
@@ -4,7 +4,10 @@
 jQuery(document).ready(function() {
     $.backstretch("./style/img/backgrounds/gaming_poker_cards.jpg");
     interesting.daytime();
+    $("#mover").hover(function() {
+        appmain.ht = 'Send Email to LLF@wmpcxpy.com for Reg.';
+    }, function() {
+        appmain.ht = 'User Sign-Up is disabled.';
+
+    });
 });
-// $(function() {
-//     $("[data-toggle='popover']").popover();
-// });
