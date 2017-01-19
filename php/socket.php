5c3daabd66c7c09077eadc8883576c3586cefa05
Meng Wang <water.wang@live.com>
2017-01-19 09:53:29 -0600

    update

--- php/socket.php
+++ php/socket.php
@@ -1,14 +1,18 @@
 <?php
-
-require_once 'X:/UwAmp/www/LandlordFighter/vendor/autoload.php';
+// update: make this common
+require_once '../vendor/autoload.php';
 use Workerman\Worker;
 use PHPSocketIO\SocketIO;
 
 // 创建socket.io服务端，监听2021端口
 $io = new SocketIO(3120);
 // 当有客户端连接时打印一行文字
+// connect
 $io->on('connection', function ($connection) use ($io) {
-  echo "new connection coming\n";
+  // point
+  $connection->on('chat message', function($msg)use($io){
+    // 触发所有客户端定义的chat message from server事件
+    $io->emit('chat message from server', $msg);
+  });
 });
-
 Worker::runAll();
--- src/connect.js
+++ src/connect.js
@@ -1,5 +1,11 @@
 var socket = io('http://127.0.0.1:3120');
 // 当连接服务端成功时触发connect默认事件
-socket.on('connect', function(){
+socket.on('connect', function() {
     console.log('connect success');
+});
+// 触发服务端的chat message事件
+socket.emit('chat message', '这个是消息内容...');
+// 服务端通过emit('chat message from server', $msg)触发客户端的chat message from server事件
+socket.on('chat message from server', function(msg) {
+    console.log('get message:' + msg + ' from server');
 });
