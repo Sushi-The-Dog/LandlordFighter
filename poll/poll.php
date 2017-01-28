8e7bb91f916243843124389f40d5c92c4e25bcdc
Meng Wang <water.wang@live.com>
2017-01-27 21:20:14 -0600

    start

--- poll/poll.php
+++ poll/poll.php
@@ -3,7 +3,20 @@ session_start();
 $username = $_SESSION['username'];
 session_write_close();
 if (pregmatch($username, '/^[a-zA-Z0-9_]+$/')) {
-    
+    $timeout = 0;
+    $re = array();
+    while($timeout < 7){
+        $list = file_get_contents('../json/poll/list.json');
+        $list = json_decode($list);
+        for($x=0;$x<count($list);$x++){
+            if($list[$x][0] == $username){
+                array_push($re,$list[0]);
+            }
+        }
+        if(count($re)>0){
+            $timeout = 10;
+        }
+    }
 }else{
     $re = 'error';
 }
