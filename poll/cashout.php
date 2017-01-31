530c2f667ab87a51ea43d43205dc19e48c39636b
Meng Wang <water.wang@live.com>
2017-01-30 19:00:18 -0600

    method

--- poll/cashout.php
+++ poll/cashout.php
@@ -8,3 +8,19 @@ function pregmatch($string, $preg)
         return false;
     }
 }
+function timeplayer($username){
+    $player = file_get_contents('../json/poll/player.json');
+    $player = json_decode($player);
+    $exist = 0;
+    for($x=0;$x<count($player);$x++){
+        if($player[$x][0] == $username){
+            $exist = 1;
+            $player[$x][1] = time();
+        }
+    }
+    if($exist == 0){
+        array_push($player,array($username,time()));
+    }
+    $player = json_encode($player);
+    file_put_contents('../json/poll/player.json', $player);
+}
--- poll/register.php
+++ poll/register.php
@@ -1,4 +1,5 @@
 <?php
+
 function pregmatch($string, $preg)
 {
     if (preg_match($preg, $string)) {
@@ -7,4 +8,20 @@ function pregmatch($string, $preg)
     else {
         return false;
     }
+}
+function timeplayer($username){
+    $player = file_get_contents('../json/poll/player.json');
+    $player = json_decode($player);
+    $exist = 0;
+    for($x=0;$x<count($player);$x++){
+        if($player[$x][0] == $username){
+            $exist = 1;
+            $player[$x][1] = time();
+        }
+    }
+    if($exist == 0){
+        array_push($player,array($username,time()));
+    }
+    $player = json_encode($player);
+    file_put_contents('../json/poll/player.json', $player);
 }
